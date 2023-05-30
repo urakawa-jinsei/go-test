60865ed20293f77ddba6bb878e7a42fc70618c0e
urakawa-jinsei <urakawa_jinsei@retail-ai.jp>
2023-05-30 11:12:07 +0900

    [update]Table Driven Tests

--- sample_test.go
+++ sample_test.go
@@ -1,13 +1,30 @@
 package hsd
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestStringDistance(t *testing.T) {
-	got := StringDistance("foo", "foh")
-	want := 1
-	if got != want {
-		t.Fatalf("expected: %v,got %v",want, got)
+	tests := []struct {
+		name string
+		lhs  string
+		rhs  string
+		want int
+	}{
+		{name: "lhs is longer than rhs", lhs: "foo", rhs: "fo", want: -1},
+		{name: "lhs is shorter than rhs", lhs: "fo", rhs: "foo", want: -1},
+		{name: "No diff", lhs: "foo", rhs: "foo", want: 0},
+		{name: "1 diff", lhs: "foo", rhs: "foh", want: 1},
+		{name: "2 diffs", lhs: "foo", rhs: "fhh", want: 2},
+		{name: "3 diffs", lhs: "foo", rhs: "hhh", want: 3},
+		{name: "multibyte", lhs: "あいう", rhs: "あいえ", want: 1},
+	}
+
+	for _, tc := range tests {
+		got := StringDistance(tc.lhs,tc.rhs)
+		if !reflect.DeepEqual(tc.want,got) {
+			t.Fatalf("%s: expected: %v,got: %v", tc.name, tc.want, got)
+		}
 	}
 }
